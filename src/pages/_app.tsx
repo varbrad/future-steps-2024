c1426633fa875ac5e867d8f6a27790e4902636f2
Brad Davies <brad.davies@withwise.com>
2024-02-06 08:29:48 +0000

    Add speed-insights & analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { trpc } from '@/server/trpc/client';
 import type { AppType } from 'next/app';
 import { Noto_Sans } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import "@/styles/globals.css";
 
@@ -10,6 +12,8 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <div className={`w-screen overflow-hidden ${notoSans.className}`}>
       <Component {...pageProps} />
+      <Analytics />
+      <SpeedInsights />
     </div>
   )
 };
